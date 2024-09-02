475282118d73c4ae43c96865c20700a885a8241f
Denis Viana <denisviana1055@gmail.com>
2024-09-02 19:24:18 -0300

    att footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,51 @@
 // header active
 const headerActive = document.querySelectorAll(".header__menu_items li a");
+const mouseEnter = document.querySelectorAll(".header__menu");
+
 function activeHeader(active) {
   const href = active.href;
   const url = location.href;
 
+  mouseEnter.forEach((mouse) => {
+    mouse.addEventListener("mouseover", (e) => {
+      active.classList.remove("active");
+    });
+  });
+  mouseEnter.forEach((mouse) => {
+    mouse.addEventListener("mouseleave", (e) => {
+      if (href.includes(url)) {
+        active.classList.add("active");
+      }
+    });
+  });
+
   if (href.includes(url)) {
     active.classList.toggle("active");
-    
   }
 }
+
 headerActive.forEach(activeHeader);
 
-  /**
-   * Easy on scroll event listener
-   */
-  const onscroll = (el, listener) => {
-    el.addEventListener("scroll", listener);
-  };
+/**
+ * Easy on scroll event listener
+ */
+const onscroll = (el, listener) => {
+  el.addEventListener("scroll", listener);
+};
 
-  
-  /**
-   * Scrolls to an element with header offset
-   */
-  const scrollto = (el) => {
-    let header = select("#header");
-    let offset = header.offsetHeight;
+/**
+ * Scrolls to an element with header offset
+ */
+const scrollto = (el) => {
+  let header = select("#header");
+  let offset = header.offsetHeight;
 
-    let elementPos = select(el).offsetTop;
-    window.scrollTo({
-      top: elementPos - offset,
-      behavior: "smooth",
-    });
-  };
+  let elementPos = select(el).offsetTop;
+  window.scrollTo({
+    top: elementPos - offset,
+    behavior: "smooth",
+  });
+};
 
 /**
  * Header fixed top on scroll
@@ -52,6 +66,6 @@ if (selectHeader) {
 }
 
 // simple anime
-if(window.SimpleAnime){
+if (window.SimpleAnime) {
   new SimpleAnime();
 }
